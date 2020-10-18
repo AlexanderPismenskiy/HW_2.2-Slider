58b57d293be599fc4f9c88c5e776fb47ac4bdae0
Alexander <alex.pismenskiy@gmail.com>
2020-10-18 15:11:52 +0300

    Start Code

--- HW_2.2 Slider/ViewController.swift
+++ HW_2.2 Slider/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var screenView: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        
     }
 
 
