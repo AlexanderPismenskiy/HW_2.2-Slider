d17728d3828ba16c4160d248f1bef70ab207ed86
Alexander <alex.pismenskiy@gmail.com>
2020-10-31 10:58:27 +0300

    Ready for HW 2.6

--- HW_2.2 Slider/ViewController.swift
+++ HW_2.2 Slider/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
     @IBOutlet var screenView: UIView!
     
     @IBOutlet var redValue: UILabel!
@@ -21,19 +22,50 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenView.layer.cornerRadius = 20
+        
+        setColor()
+        setValue(for: redValue, greenValue, blueValue)
     }
-    func changeColor() {
+    
+    
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender.tag {
+        case 0: redValue.text = string(from: sender)
+        case 1: greenValue.text = string(from: sender)
+        case 2: blueValue.text = string(from: sender)
+        default: break
+        }
+    }
+    
+    
+   private func setColor() {
         screenView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1)
-        redValue.text = String(format:"%.2f", redSlider.value)
-        greenValue.text = String(format:"%.2f", greenSlider.value)
-        blueValue.text = String(format:"%.2f", blueSlider.value)
-        }
-    @IBAction func rgbSliderChange() {
-        changeColor()
     }
+       
+  
+    private func setValue (for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redValue.text = string(from: redSlider)
+            case 1: greenValue.text = string(from: greenSlider)
+            case 2: blueValue.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+            
+    private func string(from slider: UISlider) -> String {
+                String(format: "%.2f", slider.value)
+            }
 }
+
+
 
